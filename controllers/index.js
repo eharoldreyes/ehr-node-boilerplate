2ea8eab688d06847ed7c6ce9e2c2dbc0cab03952
Harold Reyes <eharoldreyes@gmail.com>
2016-09-27 13:44:48 +0800

    Add index improter

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,14 @@
 'use strict';
 
-const fs          = require('fs');
-const path        = require('path');
-const basename    = path.basename(module.filename);
-const js     = {};
+const fs = require('fs');
+const path = require('path');
+const basename = path.basename(module.filename);
+const js = {};
 
-fs.readdirSync(__dirname)
-    .filter((file) =>  {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach((file) =>  {
-        js[file.split('.')[0]] = require(path.join(__dirname, file));
-    });
+fs.readdirSync(__dirname).filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+}).forEach(file => {
+    js[file.split('.')[0]] = require(path.join(__dirname, file));
+});
+
 module.exports = js;
